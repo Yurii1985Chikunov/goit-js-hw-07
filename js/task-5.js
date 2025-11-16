ce16017bd6feab7b051d39fa6a41616d237bb292
Yurii <hcikynov85@gmail.com>
2025-11-16 20:19:53 +0100

    tasj-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
+
+const body = document.body;
+const btn = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor; // inline-style
+  colorSpan.textContent = newColor;
+});
